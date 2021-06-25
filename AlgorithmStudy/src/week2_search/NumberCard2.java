27b99c29513c5e292210ba82ed0afa51807ce60e
hyxnskim <hskim9706@gmail.com>
2021-06-26 02:18:16 +0900

    Second solution for acmicpc.net/problem/10816
    -- Applied memoization technique

--- AlgorithmStudy/src/week2_search/NumberCard2.java
+++ AlgorithmStudy/src/week2_search/NumberCard2.java
@@ -1,6 +1,5 @@
 package week2_search;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -13,33 +12,21 @@ import java.util.Scanner;
  */
 
 public class NumberCard2 {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
 		
-		int N; // 상근이가 가지고 있는 숫자 카드의 개수
-		ArrayList<Integer> have = new ArrayList<Integer>();
-		
-		int M = 8; // 주어지는 정수의 개수
-		ArrayList<Integer> nums = new ArrayList<Integer>();
-		
-		N = sc.nextInt();
-		for(int i = 0; i < N; i++) {
-			have.add(sc.nextInt());
+		int N = sc.nextInt();
+		int[] have = new int[20000001];
+		for (int i = 0; i < N; ++i) {
+			 have[sc.nextInt()+10000000]++;
 		}
 		
-		M = sc.nextInt();
-		for(int i = 0; i < M; i++) {
-			nums.add(sc.nextInt());
-		}
-		
-		int cnt = 0, num = 0;
-		for(int i = 0; i < M; i++) {
-			cnt = 0;
-			num = nums.get(i);
-			for(int j = 0; j < N; j++) {
-				if(have.get(j) == num) cnt++;
-			}
-			System.out.print(cnt + " ");
-		}
+		int M = sc.nextInt();
+		for (int i = 0; i < M; ++i) {
+			sb.append(have[sc.nextInt()+10000000]+" ");
+		}		
+		System.out.println(sb.toString());
 	}
 }
