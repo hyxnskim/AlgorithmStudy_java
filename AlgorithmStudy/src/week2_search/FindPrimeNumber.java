3196aea2d7c72faeb6102434daf1d1adcae5d109
hyxnskim <hskim9706@gmail.com>
2021-07-02 23:33:02 +0900

    Fourth solution for programmers.co.kr/learn/courses/30/lessons/42839
    -- Solved
    -- Revised isPrime method

--- AlgorithmStudy/src/week2_search/FindPrimeNumber.java
+++ AlgorithmStudy/src/week2_search/FindPrimeNumber.java
@@ -96,7 +96,7 @@ public class FindPrimeNumber {
 	 */
 	public static boolean isPrime(int num) {
 		if(num < 2) return false;
-		for(int i = 2; i < num/2 + 1; i++) {
+		for(int i = 2; i < Math.sqrt(num); i++) {
 			if(num % i == 0) return false;
 		}
 		return true;
