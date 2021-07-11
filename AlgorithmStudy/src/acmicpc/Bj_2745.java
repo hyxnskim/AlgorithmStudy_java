1e4b4c749d9267be8230351ec645577ba1d9c149
hyxnskim <hskim9706@gmail.com>
2021-07-11 18:13:28 +0900

    My solution for acmicpc.net/problem/2745

---
+++ AlgorithmStudy/src/acmicpc/Bj_2745.java
@@ -0,0 +1,43 @@
+package acmicpc;
+
+import java.util.Scanner;
+
+public class Bj_2745 {
+	
+	/**
+	 * <pre>
+	 * My solution for https://www.acmicpc.net/problem/2745
+	 * </pre>
+	 * @author Hyunsoo Kim
+	 * @version ver.1.0
+	 * @since jdk1.8
+	 */
+	
+	public static int toDec(String origin, int numeral) {
+		int len = origin.length();
+		int result = 0;
+
+		for(int i = 0; i < len; i++) {
+			char now = origin.charAt(i);
+			int nowInt;
+		
+			if(now >= '0' && now <= '9') {
+				nowInt = now - '0';
+			} else {
+				nowInt = (now - 'A') + 10;
+			}
+			result += nowInt * Math.pow(numeral, len - i - 1);
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String N = sc.next();
+		int B = sc.nextInt();
+		System.out.println(toDec(N, B));
+		
+		sc.close();
+	}
+}
