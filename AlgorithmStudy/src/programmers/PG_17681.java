dfa92714042072a086da5fbefdbb72406d222a1a
hyxnskim <hskim9706@gmail.com>
2021-07-16 00:12:01 +0900

    My second solution for programmers.co.kr/learn/courses/30/lessons/17681
    -Solved

--- AlgorithmStudy/src/programmers/PG_17681.java
+++ AlgorithmStudy/src/programmers/PG_17681.java
@@ -12,10 +12,13 @@ public class PG_17681 {
 	 */
 
 	public static void main(String[] args) {
-		int n = 5;
-		int[] arr1 = {9, 20, 28, 18, 11};
-		int[] arr2 = {30, 1, 21, 17, 28};
+		int n = 6;
+		int[] arr1 = {46, 33, 33 ,22, 31, 50};
+		int[] arr2 = {27 ,56, 19, 14, 14, 10};
 		String[] answer = solution(n, arr1, arr2);
+		
+		System.out.println("22 | 14 = " + (22|14));
+		System.out.println("31 | 14 = " + (31|14));
 		
 		for(int i = 0; i < n; i++) {
 			System.out.println(answer[i]);
@@ -27,15 +30,13 @@ public class PG_17681 {
         
         for(int i = 0; i < n; i++) {
         	int[] tmp = decToBin(arr1[i], arr2[i], n);
-        	String str = " ";
+        	StringBuilder str = new StringBuilder();
         	for(int j = 0; j < n; j++) {
-        		if(tmp[j] == 1) str.concat("#");
-        		else str.concat(" ");
+        		if(tmp[j] == 1) str.append("#");
+        		else str.append(" ");
         	}
-        	answer[i] = str;
-        	System.out.println(str);
+        	answer[i] = str.toString();
         }
-        
         return answer;
     }
 	
@@ -44,13 +45,10 @@ public class PG_17681 {
 		int num = a | b;
 		
 		int idx = size -1;
-		while(num > 1) {
+		for(int i = size - 1; i >= 0; i--) {
 			res[idx--] = num % 2;
 			num /= 2;
 		}
-		res[0] = num;
-		
 		return res;
 	}
-
 }
