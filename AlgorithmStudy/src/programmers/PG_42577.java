42b44557c1170cefb4580387fb73c4e70e6d8aff
hyxnskim <hskim9706@gmail.com>
2021-07-21 23:13:45 +0900

    My second solution for programmers.co.kr/learn/courses/30/lessons/42577
    -Timeout failure

--- AlgorithmStudy/src/programmers/PG_42577.java
+++ AlgorithmStudy/src/programmers/PG_42577.java
@@ -19,12 +19,13 @@ public class PG_42577 {
 	 public static boolean solution(String[] phone_book) {
         boolean answer = true;
         
+        Loop:
         for(int i = 0; i < phone_book.length; i++) {
         	for(int j = 0; j < phone_book.length; j++) {
         		if(i == j) continue;
         		if(phone_book[i].startsWith(phone_book[j])) {
         			answer = false;
-        			break;
+        			break Loop;
         		}
         	}
         }
