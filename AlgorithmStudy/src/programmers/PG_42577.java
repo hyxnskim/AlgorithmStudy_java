a13b870fd17c5fd592ce2718f3962bf769dc037c
hyxnskim <hskim9706@gmail.com>
2021-07-22 23:41:27 +0900

    My sixth solution for programmers.co.kr/learn/courses/30/lessons/42577
    -Changed to use hashset
    -Correct finally..

--- AlgorithmStudy/src/programmers/PG_42577.java
+++ AlgorithmStudy/src/programmers/PG_42577.java
@@ -1,6 +1,6 @@
 package programmers;
 
-import java.util.Arrays;
+import java.util.HashSet;
 
 public class PG_42577 {
 	
@@ -20,17 +20,18 @@ public class PG_42577 {
 	
 	 public static boolean solution(String[] phone_book) {
         boolean answer = true;
+
+        HashSet<String> set = new HashSet<>();
+        for(String p : phone_book) set.add(p);
         
-        Arrays.sort(phone_book);
-        
-        loop:
-        for(int i = 0; i < phone_book.length && answer; i++) {
-        	for(int j = i+1 ; j < phone_book.length; j++) {
-        		if(phone_book[j].startsWith(phone_book[i])) {
-        			answer = false;
-        			break loop;
-        		}
-        	}
+        Loop :
+        for(String p : phone_book){
+            for(int i = 0; i < p.length(); i++) {             
+                if(set.contains(p.substring(0,i))) {
+                	answer = false; 
+                	break Loop;
+                }
+            }
         }
         return answer;
 	 }
