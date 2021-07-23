b40d2c1f6d8f1427d1db9bd3436a611b026b29b0
hyxnskim <hskim9706@gmail.com>
2021-07-23 16:25:05 +0900

    My first solution for acmicpc.net/problem/10872

---
+++ AlgorithmStudy/src/acmicpc/Bj_10872.java
@@ -0,0 +1,29 @@
+package acmicpc;
+
+import java.util.Scanner;
+
+public class Bj_10872 {
+	
+	/**
+	 * <pre>
+	 * My solution for https://www.acmicpc.net/problem/10872
+	 * </pre>
+	 * @author Hyunsoo Kim
+	 * @version ver.1.0
+	 * @since jdk1.8
+	 */
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int res = 1;
+		for(int i = 2; i <= N; i++) {
+			res *= i;
+		}
+		System.out.println(res);
+		
+		sc.close();
+	}
+
+}
