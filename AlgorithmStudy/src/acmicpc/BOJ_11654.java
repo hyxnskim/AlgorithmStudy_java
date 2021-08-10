1dbe3a5151e90c91f8e97e939db27dcf45cff6bb
hyxnskim <hskim9706@gmail.com>
2021-08-10 23:48:09 +0900

    My first solution for acmicpc.net/problem/11654

---
+++ AlgorithmStudy/src/acmicpc/BOJ_11654.java
@@ -0,0 +1,24 @@
+package acmicpc;
+
+/**
+ * <pre>
+ * My solution for https://www.acmicpc.net/problem/11654
+ * </pre>
+ * @author Hyunsoo Kim
+ * @version ver.1.0
+ * @since jdk1.8
+ */
+
+import java.util.Scanner;
+
+public class BOJ_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		
+		System.out.printf("%d", (int)c);
+		
+		sc.close();
+	}
+}
