2c774e4a618f1cd9e1831f515ad427d795189d76
hyxnskim <hskim9706@gmail.com>
2021-08-14 19:45:32 +0900

    My second solution for acmicpc.net/problem/2941
    --Solved

--- AlgorithmStudy/src/acmicpc/BOJ_2941.java
+++ AlgorithmStudy/src/acmicpc/BOJ_2941.java
@@ -17,19 +17,20 @@ public class BOJ_2941 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.next();
+		int len = str.length();
 		
 		int cnt = 0;
-		for(int i = 0; i < str.length(); i++) {
+		for(int i = 0; i < len; i++) {
 			char c = str.charAt(i);
 			
 			if(c == 'c') {
-				if(str.charAt(i+1) == '=' || str.charAt(i+1) == '-') i++;
+				if(i+1 < len && (str.charAt(i+1) == '=' || str.charAt(i+1) == '-')) i++;
 			} else if(c == 'd') {
-				if(str.charAt(i+1) == 'z' && str.charAt(i+2) == '=') i += 2;
-				else if(str.charAt(i+1) == '-') i++;
-			} else if((c == 'l' || c == 'n') && str.charAt(i+1) == 'j') {
+				if(i+2 < len && str.charAt(i+1) == 'z' && str.charAt(i+2) == '=') i += 2;
+				else if(i+1 < len && str.charAt(i+1) == '-') i++;
+			} else if((c == 'l' || c == 'n') && i+1 < len && str.charAt(i+1) == 'j') {
 				i++;
-			} else if((c == 's' || c =='z') && str.charAt(i+1) == '=' ) {
+			} else if((c == 's' || c =='z') && i+1 < len && str.charAt(i+1) == '=' ) {
 				i++;
 			}
 			cnt++;
