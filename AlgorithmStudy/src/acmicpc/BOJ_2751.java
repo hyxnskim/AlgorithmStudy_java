fb40b930367cdc96eed365e997fa4614fd7c2bb7
hyxnskim <hskim9706@gmail.com>
2021-08-28 00:48:27 +0900

    My second solution for acmicpc.net/problem/2751
    - Used merge sort
    - Timeout error

--- AlgorithmStudy/src/acmicpc/BOJ_2751.java
+++ AlgorithmStudy/src/acmicpc/BOJ_2751.java
@@ -9,27 +9,62 @@ package acmicpc;
  * @since jdk1.8
  */
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class BOJ_2751 {
+	
+	static int N;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int N = sc.nextInt();
-		List<Integer> list = new ArrayList<>();
+		N = sc.nextInt();
+		int[] list = new int[N];
 		
 		for(int i = 0; i < N; i++) {
-			list.add(sc.nextInt());
+			list[i] = sc.nextInt();
 		}
-		
-		list.sort(null);
+	
+		merge_sort(list, 0, N);
 		
 		for(int i = 0; i < N; i++) {
-			System.out.println(list.get(i));
+			System.out.println(list[i]);
 		}
 		
 		sc.close();
+	}
+	
+	public static void merge_sort(int list[], int left, int right) {
+		if(right - left > 1) {
+			int mid = (left + right) / 2;
+			merge_sort(list, left, mid);
+			merge_sort(list, mid, right);
+			merge(list, left, mid, right);
+		}
+	}
+	
+	public static void merge(int list[], int left, int mid, int right) {
+		int[] tmp = new int[N];
+		int i = left, j = mid;
+		int idx = left;
+		
+		while(i < mid && j < right) {
+			if(list[i] < list[j]) {
+				tmp[idx++] = list[i++];
+			} else {
+				tmp[idx++] = list[j++];
+			}
+		}
+		
+		while(i < mid) {
+			tmp[idx++] = list[i++];
+		}
+		while(j < right) {
+			tmp[idx++] = list[j++];
+		}
+		
+		for(i = left; i < right; i++) {
+			list[i] = tmp[i];
+		}
 	}
 }
