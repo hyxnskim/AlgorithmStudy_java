532c726c1b1011227715c4407c65fd510744a010
hyxnskim <hskim9706@gmail.com>
2021-09-03 09:26:44 +0900

    My second solution for SWEA_1221
    -Reduced time and memory

--- AlgorithmStudy/src/swea/N1220_1229/SWEA_1221_GNS.java
+++ AlgorithmStudy/src/swea/N1220_1229/SWEA_1221_GNS.java
@@ -10,12 +10,12 @@ package swea.N1220_1229;
  * @since jdk1.8
  */
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class SWEA_1221_GNS {
 	
 	static String[] nums = {"ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"};
+	static int[] cnt;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -24,12 +24,10 @@ public class SWEA_1221_GNS {
 		for(int t = 0; t < T; t++) {
 			String[] input = sc.nextLine().split(" ");
 			int N = Integer.parseInt(input[1]);
-			System.out.println();
-			System.out.println("N = " + N);
+			cnt = new int[10];
 			
 			input = sc.nextLine().split(" ");
-	
-			int[] arr = new int[N];
+
 			int tmp = -1;
 			for(int i = 0; i < N; i++) {
 				switch(input[i]) {
@@ -44,15 +42,16 @@ public class SWEA_1221_GNS {
 					case "EGT" : tmp = 8; break;
 					case "NIN" : tmp = 9; break;
 				}
-				arr[i] = tmp;
+				cnt[tmp]++;
 			}
 			
-			Arrays.sort(arr);
-			
 			System.out.printf("#%d\n", t+1);
-			for(int i = 0; i < N; i++) System.out.print(nums[arr[i]] + " ");
+			for(int i = 0; i < 10; i++) 
+				for(int j = 0; j < cnt[i]; j++)
+					System.out.print(nums[i] + " ");
 		}
 		
 		sc.close();
 	}
+	
 }
