90e434abc63c870c5058969f612d615a6a63060c
hyxnskim <hskim9706@gmail.com>
2021-09-06 13:51:05 +0900

    My second solution for SWEA_4008
    -Changed permutation to dfs
    -Reduced runtime

--- AlgorithmStudy/src/swea/N4000_4009/SWEA_4008_숫자만들기.java
+++ AlgorithmStudy/src/swea/N4000_4009/SWEA_4008_숫자만들기.java
@@ -29,14 +29,9 @@ public class SWEA_4008_숫자만들기 {
 			min = Integer.MAX_VALUE;
 			max = Integer.MIN_VALUE;
 			
-			oper = new int[N-1];
-			tr = new int[N-1];
-			int idx = 0;
+			oper = new int[4];
 			for(int i = 0; i < 4; i++) {
-				int tmp = sc.nextInt();
-				for(int j = 0; j < tmp; j++) {
-					oper[idx++] = i;
-				}
+				oper[i] = sc.nextInt();
 			}
 			
 			nums = new int[N];
@@ -44,42 +39,29 @@ public class SWEA_4008_숫자만들기 {
 				nums[i] = sc.nextInt();
 			}
 			
-			perm(N-1, N-1);
+			dfs(nums[0], 1);
 			
 			System.out.printf("#%d %d\n", t+1, max - min);
 		}
 		sc.close();
 	}
 	
-	static void swap(int i, int j) {
-		int t = oper[i];  oper[i] = oper[j]; oper[j] = t;
-	}
-	
-	static void cal_arr() {
-		int res = nums[0];
-		for (int i = 0; i < N-1; i++) {
-			switch(tr[i]) {
-			case 0: res += nums[i+1]; break;
-			case 1: res -= nums[i+1]; break;
-			case 2: res *= nums[i+1]; break;
-			case 3: res /= nums[i+1]; break;
+	public static void dfs(int res, int idx) {
+		if(idx == N) {
+			if(res < min) min = res;
+			if(res > max) max = res;
+		}
+		
+		for(int i = 0; i < 4; i++) {
+			if(oper[i] != 0) {
+				oper[i]--;
+				if(i == 0) dfs(res + nums[idx], idx+1);
+				else if(i == 1) dfs(res - nums[idx], idx+1);
+				else if(i == 2) dfs(res * nums[idx], idx+1);
+				else if(i == 3) dfs(res / nums[idx], idx+1);
+				oper[i]++;
 			}
 		}
-		if(res < min) min = res;
-		if(res > max) max = res;
 	}
 
-	
-	static void perm(int n, int r) {
-		if (r == 0) cal_arr();
-		else {
-			for (int i = n - 1; i >= 0; i--) {
-				if(i != n-1 && oper[i] == oper[n-1]) continue;
-				swap(i, n - 1);
-				tr[r - 1] = oper[n - 1];
-				perm(n - 1, r - 1);
-				swap(i, n - 1);
-			}
-		}
-	}
 }
