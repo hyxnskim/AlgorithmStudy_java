8bbe2276904c50e9ac55e3c7a1447f209b55e059
hyxnskim <hskim9706@gmail.com>
2021-09-19 17:21:04 +0900

    My third solution for acmicpc.net/problem/2751
    -Solved
    -Changed merge sort to Collection.sort

--- AlgorithmStudy/src/acmicpc/BOJ_2751.java
+++ AlgorithmStudy/src/acmicpc/BOJ_2751.java
@@ -9,62 +9,40 @@ package acmicpc;
  * @since jdk1.8
  */
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class BOJ_2751 {
 	
 	static int N;
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		N = sc.nextInt();
-		int[] list = new int[N];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<>();
 		
 		for(int i = 0; i < N; i++) {
-			list[i] = sc.nextInt();
+			list.add(Integer.parseInt(br.readLine()));
 		}
 	
-		merge_sort(list, 0, N);
+		Collections.sort(list);
 		
-		for(int i = 0; i < N; i++) {
-			System.out.println(list[i]);
+		StringBuilder sb = new StringBuilder();
+		for(int i : list) {
+			sb.append(i + "\n");
 		}
+		bw.write(sb.toString());
 		
-		sc.close();
+		br.close();
+		bw.close();
 	}
 	
-	public static void merge_sort(int list[], int left, int right) {
-		if(right - left > 1) {
-			int mid = (left + right) / 2;
-			merge_sort(list, left, mid);
-			merge_sort(list, mid, right);
-			merge(list, left, mid, right);
-		}
-	}
-	
-	public static void merge(int list[], int left, int mid, int right) {
-		int[] tmp = new int[N];
-		int i = left, j = mid;
-		int idx = left;
-		
-		while(i < mid && j < right) {
-			if(list[i] < list[j]) {
-				tmp[idx++] = list[i++];
-			} else {
-				tmp[idx++] = list[j++];
-			}
-		}
-		
-		while(i < mid) {
-			tmp[idx++] = list[i++];
-		}
-		while(j < right) {
-			tmp[idx++] = list[j++];
-		}
-		
-		for(i = left; i < right; i++) {
-			list[i] = tmp[i];
-		}
-	}
 }
