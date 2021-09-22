ba35bc91e04a19d3096c01f4b103d1e97b36692b
hyxnskim <43996407+hyxnskim@users.noreply.github.com>
2021-09-22 22:55:43 +0900

    My first solution for acmicpc.net/problem/11653
    - Timeout Error

---
+++ AlgorithmStudy/src/acmicpc/BOJ_11653.java
@@ -0,0 +1,45 @@
+/**
+ * <pre>
+ * My solution for https://www.acmicpc.net/problem/11653
+ * </pre>
+ * @author Hyunsoo Kim
+ * @version ver.1.0
+ * @since jdk1.8
+ */
+
+import java.util.Scanner;
+
+public class BOJ_11653 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int idx = N;
+		
+		if(isPrime(N)) System.out.println(N);
+		else if(N == 1);
+		else {
+			
+			for(int i = 2; i <= idx/2; i++) {
+				if(!isPrime(i) || N%i != 0) continue;
+				
+				while(N >= i && N%i == 0) {
+					System.out.println(i);
+					N /= i;
+				}
+			}
+		}
+		
+		sc.close();
+	}
+	
+	static boolean isPrime(int n) {
+		if(n <= 1) return false;
+		for(int i = 2; i <= n/2; i++) {
+			if(n%i == 0) return false;
+		}
+		return true;
+	}
+
+}
