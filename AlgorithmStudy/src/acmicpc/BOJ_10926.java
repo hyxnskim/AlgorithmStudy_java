871b72645e64e22dae94f8c2616740151eea809b
hyxnskim <43996407+hyxnskim@users.noreply.github.com>
2022-03-30 23:56:00 +0900

    My first solution for acmicpc.net/problem/10926

---
+++ AlgorithmStudy/src/acmicpc/BOJ_10926.java
@@ -0,0 +1,21 @@
+/**
+ * <pre>
+ * My solution for https://www.acmicpc.net/problem/10926
+ * </pre>
+ * @author Hyunsoo Kim
+ * @version ver.1.0
+ * @since jdk1.8
+ */
+import java.util.Scanner;
+
+public class BOJ_10926 {
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+
+		String input = sc.nextLine();
+		input = input.concat("??!");
+		System.out.println(input);
+
+		sc.close();
+	}
+}
