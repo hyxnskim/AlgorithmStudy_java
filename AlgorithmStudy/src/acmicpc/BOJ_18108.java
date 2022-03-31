b4f56752c242245dbbdde04e6709dbbdca563780
hyxnskim <43996407+hyxnskim@users.noreply.github.com>
2022-03-31 23:59:55 +0900

    My first solution for acmicpc.net/problem/18108

---
+++ AlgorithmStudy/src/acmicpc/BOJ_18108.java
@@ -0,0 +1,18 @@
+/**
+ * <pre>
+ * My solution for https://www.acmicpc.net/problem/18108
+ * </pre>
+ * @author Hyunsoo Kim
+ * @version ver.1.0
+ * @since jdk1.8
+ */
+
+import java.util.Scanner;
+
+public class BOJ_18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()-543);
+		sc.close();
+	}
+}
