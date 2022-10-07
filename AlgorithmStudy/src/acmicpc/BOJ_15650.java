9ffc32f173f51fb08cc3bd6f19967bd2f3af4b7e
hyxnskim <43996407+hyxnskim@users.noreply.github.com>
2022-10-08 01:47:43 +0900

    Create BOJ_15650.java

---
+++ AlgorithmStudy/src/acmicpc/BOJ_15650.java
@@ -0,0 +1,49 @@
+package acmicpc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15650 {
+	
+	static int N, M;
+	static boolean[] isVisited;
+	static int[] arr;
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		isVisited = new boolean[N];
+		arr = new int[M];
+		
+		dfs(0);
+	}
+	
+	public static void dfs(int depth) {
+		
+		if(depth == M) {
+			for(int i : arr) System.out.print(i + " ");
+			System.out.println();
+			return;
+		}
+		
+		for(int i = 0; i < N; i++) {
+			if(!isVisited[i]) {
+				if(depth == 0 || i+1 > arr[depth-1]) {
+					isVisited[i] = true;
+					arr[depth] = i+1;
+					dfs(depth+1);
+					isVisited[i] = false;
+				}
+			}
+			
+		}
+		
+	}
+
+}
