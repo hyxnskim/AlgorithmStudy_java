ce37238063ee12446473d1493afcb012445967bb
hyxnskim <43996407+hyxnskim@users.noreply.github.com>
2022-10-15 00:42:32 +0900

    Update Samsung_2022_ws_pm_2.java
    7번테케 틀림.......

--- AlgorithmStudy/src/codetree/Samsung_2022_ws_pm_2.java
+++ AlgorithmStudy/src/codetree/Samsung_2022_ws_pm_2.java
@@ -86,6 +86,7 @@ public class Samsung_2022_ws_pm_2 {
 	}
 	
 	public static void kill() {
+		if(killPos[0] == -1 && killPos[1] == -1) return;
 		
 		cnt += mat[killPos[0]][killPos[1]];
 		mat[killPos[0]][killPos[1]] = -2 * (c+1);
@@ -115,6 +116,7 @@ public class Samsung_2022_ws_pm_2 {
 	}
 	
 	public static void findPos() {
+		killPos[0] = -1; killPos[1] = -1;
 		int max = 0;
 
 		for(int i = 0; i < n; i++) {
