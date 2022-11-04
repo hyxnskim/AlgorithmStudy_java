c36e43b20e23d5b09a3d8354df4065606874ea32
hyxnskim <43996407+hyxnskim@users.noreply.github.com>
2022-11-05 00:55:09 +0900

    반복문

--- AlgorithmStudy/src/acmicpc/BOJ_11053.java
+++ AlgorithmStudy/src/acmicpc/BOJ_11053.java
@@ -18,17 +18,14 @@ public class BOJ_11053 {
 		for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
 		
 		dp = new int[N];
-		int max = 0;
-		dp[0] = 1;
-		
-		for(int i = 1; i < N; i++) {
+		for(int i = 0; i < N; i++) {
+			dp[i] = 1;
 			for(int j = 0; j < i; j++) {
-				if(arr[j] < arr[i] && arr[j] > arr[max]) max = j;
+				if(arr[j] < arr[i] && dp[i] < dp[j]+1) dp[i] = dp[j] + 1;
 			}
-			dp[i] = dp[max] + 1;
 		}
 		
-		max = 0;
+		int max = 0;
 		for(int i = 0; i < N; i++) {
 			if(dp[i] > max) max = dp[i];
 		}
